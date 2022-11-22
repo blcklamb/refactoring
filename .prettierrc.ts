f6f08f03d385461672f76369de7a2ce006c34564
chaejung Kim <whenucan35@gmail.com>
2022-11-22 22:14:16 +0900

    fix: eslint, prettier config

--- .prettierrc.ts
+++ .prettierrc.ts
@@ -3,7 +3,6 @@ module.exports = {
   tabWidth: 4,
   semi: false,
   singleQuote: true,
-  bracketSameLine: false,
   bracketSpacing: true,
   printWidth: 80,
   proseWrap: "preserve",
@@ -11,4 +10,5 @@ module.exports = {
   requirePragma: false,
   vueIndentScriptAndStyle: false,
   embeddedLanguageFormatting: "auto",
+  useTabs: false,
 };
