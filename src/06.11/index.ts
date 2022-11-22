02cdafebd067ca3034acf6d9be5880eabe9abdb4
chaejung Kim <whenucan35@gmail.com>
2022-11-22 22:15:19 +0900

    step0: initial code - 앞의 코드는 상품 정보로 상품 가격 계산 - 뒤의 코드는 배송 정보로 결제 금액 중 배송비 계산

--- src/06.11/index.ts
+++ src/06.11/index.ts
@@ -1,7 +1,48 @@
-type Props = {};
+interface Props {
+  product: {
+    basePrice: number;
+    discountThreshold: number;
+    discountRate: number;
+  };
+  quantity: number;
+  shippingMethod: {
+    discountThreshold: number;
+    discountedFee: number;
+    feePerCase: number;
+  };
+}
 
-const index = (props: Props) => {
-  return props;
+const mockData = {
+  product: {
+    basePrice: 15000,
+    discountThreshold: 1000,
+    discountRate: 70,
+  },
+  quantity: 500,
+  shippingMethod: {
+    discountThreshold: 200,
+    discountedFee: 10,
+    feePerCase: 50,
+  },
+};
+
+const priceOrder = ({ product, quantity, shippingMethod }: Props) => {
+  const basePrice = product.basePrice * quantity;
+  const discount =
+    Math.max(quantity - product.discountThreshold, 0) *
+    product.basePrice *
+    product.discountRate;
+  const shippingPerCase =
+    basePrice > shippingMethod.discountThreshold
+      ? shippingMethod.discountedFee
+      : shippingMethod.feePerCase;
+  const shippingCost = quantity * shippingPerCase;
+  const price = basePrice - discount + shippingCost;
+  return price;
+};
+
+const index = () => {
+  return priceOrder(mockData);
 };
 
 export default index;
--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,3 @@
 import index from "./06.11/index";
 
-let a = "hello";
-console.log(index(a));
+console.log(index());
