e5f16ac1232c80c0370d067c7a12d6c1bdebb04c
chaejung Kim <whenucan35@gmail.com>
2022-11-27 20:14:14 +0900

    step1: 변수 캡슐화하기(6.06)

--- src/07.01/index.ts
+++ src/07.01/index.ts
@@ -1,6 +1,10 @@
 const organization = { name: "애크미 구스베리", country: "GB" };
 
+function getRawDataOfOrganization() {
+  return organization;
+}
+
 // 읽기 예
-const result = `<h1>${organization.name}</h1>`;
+const result = `<h1>${getRawDataOfOrganization().name}</h1>`;
 // 쓰기 예
-organization.name = "무지 타이거";
+getRawDataOfOrganization().name = "무지 타이거";
