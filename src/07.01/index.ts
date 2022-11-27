dd3cce8c588e5048df7629d54899cd0750f8a75c
chaejung Kim <whenucan35@gmail.com>
2022-11-27 20:25:32 +0900

    step2: 클래스로 해당 변수의 내용을 교체 - 클래스에 게터, 세터 새로 생성

--- src/07.01/index.ts
+++ src/07.01/index.ts
@@ -1,10 +1,37 @@
-const organization = { name: "애크미 구스베리", country: "GB" };
+interface classProps {
+  name: string;
+  country: string;
+}
+class Organization {
+  _data: classProps;
+
+  constructor(data: classProps) {
+    this._data = data;
+  }
+
+  set name(aString) {
+    this._data.name = aString;
+  }
+
+  get name() {
+    return this._data.name;
+  }
+}
+
+const organization = new Organization({
+  name: "애크미 구스베리",
+  country: "GB",
+});
 
 function getRawDataOfOrganization() {
+  return organization._data;
+}
+
+function getOrganization() {
   return organization;
 }
 
 // 읽기 예
-const result = `<h1>${getRawDataOfOrganization().name}</h1>`;
+const result = `<h1>${getOrganization().name}</h1>`;
 // 쓰기 예
-getRawDataOfOrganization().name = "무지 타이거";
+getOrganization().name = "무지 타이거";
